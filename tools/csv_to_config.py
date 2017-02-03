b61d6e0346fcfdc0a7ab537e8ec1b6d67f7b6a70
glgerard <glgerard@gmail.com>
2017-02-03 21:11:03 +0100

    Bug fixes.

--- tools/csv_to_config.py
+++ tools/csv_to_config.py
@@ -2,7 +2,8 @@ from __future__ import print_function
 import sys
 import json
 import csv
-from hashlib import md5
+import copy
+from ast import literal_eval
 
 def main(argv):
     configCsvFile = argv[0]
@@ -11,29 +12,43 @@ def main(argv):
     with open(configJsonFile,'r') as jsonfile:
         templateConfig = json.load(jsonfile)
 
+
     with open(configCsvFile, 'r') as csvfile:
         csvreader = csv.DictReader(csvfile, delimiter=',')
         for row in csvreader:
-            templateConfig["seed"] = row["seed"]
-            templateConfig["runs"] = row["runs"]
+            print(row)
 
-            for net in templateConfig['dbns'].keys()+['top']:
+            config = copy.deepcopy(templateConfig)
+
+            config["seed"] = literal_eval(row["seed"])
+            config["runs"] = literal_eval(row["runs"])
+
+            pathways=[]
+
+            for net in config['dbns'].keys()+['top']:
                 lcNet = net.lower()
-                if row[lcNet+'.active'] == "False":
-                    break
-                if net != 'top':
-                    netConfig = templateConfig['dbns'][net]
-                else:
-                    netConfig = templateConfig[net]
-                for (key,value) in netConfig.items():
-                    colL2 = lcNet + '.' + key.lower()
-                    if isinstance(value, list):
-                        for i, _ in enumerate(value):
-                            colL3 = colL2 + '.' + str(i)
-                            value[i] = row[colL3]
+                print(literal_eval(row[lcNet+'.active']))
+                if literal_eval(row[lcNet+'.active']):
+                    if net != 'top':
+                        pathways.append(net)
+                    if net != 'top':
+                        netConfig = config['dbns'][net]
                     else:
-                        netConfig[key] = row[colL2]
-            print(templateConfig)
+                        netConfig = config[net]
+                    for (key,value) in netConfig.items():
+                        colL2 = lcNet + '.' + key.lower()
+                        if isinstance(value, list):
+                            for i, _ in enumerate(value):
+                                colL3 = colL2 + '.' + str(i)
+                                value[i] = literal_eval(row[colL3])
+                        else:
+                            try:
+                                netConfig[key] = literal_eval(row[colL2])
+                            except:
+                                netConfig[key] = row[colL2]
+            config['pathways'] = pathways
+            with open(row['config_file'], 'w') as f:
+                json.dump(config, f, indent=4, sort_keys=True)
 
     return
 
