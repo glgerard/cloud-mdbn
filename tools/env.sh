5d1801f0d8f182cd142844c8293588aff4db2502
Gianluca Gerard <glgerard@gmail.com>
2017-02-21 10:24:29 +0100

    Notebooks with the analysis of the various configurations.

--- tools/env.sh
+++ tools/env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export MDBN_ROOT=/home/gluca/SoftwareProjects/Thesis/cloud-mdbn
+export MDBN_ROOT=${HOME}/SoftwareProjects/Thesis/cloud-mdbn
 export PYTHONPATH=${PYTHONPATH}:${MDBN_ROOT}/src
 export PATH=${MDBN_ROOT}/tools:$PATH
--- tools/search_run.sh
+++ tools/search_run.sh
@@ -3,15 +3,16 @@
 # Receives as input the search pattern which is typically a portion of the
 # short name of the configuration file
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <patter>"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <ov|aml> <patter>"
     exit -1
 fi
 
-pattern=$1
+project=$1
+pattern=$2
 
-grep $pattern ov_configs.csv | awk -F',' -v count=1 '{print count++,")",$1,$(NF)}'
-matches=$(grep $pattern ov_configs.csv | awk -F',' '{print $(NF)}')
+grep $pattern ${project}_configs.csv | awk -F',' -v count=1 '{print count++,")",$1,$(NF)}'
+matches=$(grep $pattern ${project}_configs.csv | awk -F',' '{print $(NF)}')
 
 echo
 echo -n "Choose config file: "
