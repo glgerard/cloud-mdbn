00ae0dc5b7acbc1baab1301c11c7a1253a9b0421
Gianluca Gerard <glgerard@gmail.com>
2017-02-26 22:46:26 +0100

    Fixed awscli setup.

--- tools/aws_setup.sh
+++ tools/aws_setup.sh
@@ -3,6 +3,8 @@
 # Setup the EC2 instance and start the batch
 
 sudo apt-get -q -y install g++
+sudo apt-get -q -y install python-qt4
+sudo apt-get -q -y install awscli
 sudo mkfs /dev/xvdk
 sudo mkdir /mnt/db
 sudo mount /dev/xvdk /mnt/db
@@ -10,7 +12,7 @@ sudo chown ubuntu:ubuntu /mnt/db
 wget -q https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 source $HOME/miniconda/bin/activate
-pip install --quiet --upgrade --user awscli
+#pip install --quiet --upgrade --user awscli
 conda install -y -q boto3
 conda install -y -q numpy
 conda install -y -q scipy
@@ -18,7 +20,6 @@ conda install -y -q theano
 conda install -y -q pyqt
 conda install -y -q matplotlib
 conda install -y -q flask
-sudo apt-get -q -y install python-qt4
 cd /mnt/db
 git clone https://github.com/glgerard/cloud-mdbn.git
 cd cloud-mdbn/
