83a2e88d4e2a49961b7a5f838809b9c28e08de02
Gianluca Gerard <glgerard@gmail.com>
2017-03-10 10:45:25 +0100

    Delete query-db.sh

--- tools/query-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-host="taurus"
-port=8000
-
-uuid=$1
-
-aws dynamodb query --table-name jobs --key-conditions "{\"job\": { \"AttributeValueList\": [ { \"S\": \"$uuid\"} ], \"ComparisonOperator\": \"EQ\"}}" --endpoint-url http://$host:$port
